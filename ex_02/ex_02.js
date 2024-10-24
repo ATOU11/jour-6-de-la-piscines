cbcb5eb96e6bd21e852446c05f0e196c516756d6
ATOU11 <prince.atou@epitech.digital>
2024-10-24 09:50:44 +0200

    Update ex_02.js

--- ex_02/ex_02.js
+++ ex_02/ex_02.js
@@ -1,4 +1,4 @@
-// definie une fonction addcar dans cette fonction j'entre deux paramètre 
+// on definie une fonction addcar qui prend  deux paramètres 
 function addCar(agentString, car) {
 // ici je déclare une constante nommée agent qui va contenir l'objet après le parsing
   const agent = JSON.parse(agentString);
